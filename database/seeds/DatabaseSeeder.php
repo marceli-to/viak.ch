5a9c9de5395a6c7f6e69c04ef60c14b5604fd4aa
Marcel Stadelmann <m@marceli.to>
2022-07-05 08:07:15 +0200

    add models and seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,24 @@
 <?php
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call([
+      \Database\Seeders\RoleSeeder::class,
+      \Database\Seeders\AdminSeeder::class,
+      \Database\Seeders\ExpertSeeder::class,
+      \Database\Seeders\StudentSeeder::class,
+      \Database\Seeders\CategorySeeder::class,
+      \Database\Seeders\SoftwareSeeder::class,
+      \Database\Seeders\TagSeeder::class,
+      \Database\Seeders\LocationSeeder::class
+    ]);
+  }
 }
