0c35e8905fec1ead1014bbee1f5b83659725738a
Marcel Stadelmann <m@marceli.to>
2022-07-18 01:14:23 +0200

    bf

--- resources/views/web/partials/head.blade.php
+++ resources/views/web/partials/head.blade.php
@@ -2,8 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @if(trim($__env->yieldContent('html_class'))) class="@yield('html_class')"@endif>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@if(trim($__env->yieldContent('seo_title')))@yield('seo_title') &bull; {{config('seo.title')}}@else{{config('seo.title')}}@endif</title>
+<meta name="description" content="@if(trim($__env->yieldContent('seo_description')))@yield('seo_description')@else{{config('seo.description')}}@endif">
+<meta name="keywords" content="{{config('seo.keywords')}}">
+<meta property="og:title" content="@if(trim($__env->yieldContent('seo_title')))@yield('seo_title') &bull; {{config('seo.title')}}@else{{config('seo.title')}}@endif">
+<meta property="og:description" content="@if(trim($__env->yieldContent('seo_description')))@yield('seo_description')@else{{config('seo.description')}}@endif">
+<meta property="og:url" content="{{url()->current()}}">
+<meta property="og:image" content="@if(trim($__env->yieldContent('og_image')))@yield('og_image')@else{{ asset('assets/img/forum-architektur-og.jpg') }}@endif">
+<meta property="og:site_name" content="{{config('seo.title')}}">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
