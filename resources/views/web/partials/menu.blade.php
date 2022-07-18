fea2a138a57b97c3a6031f2a07e20d457116a434
Marcel Stadelmann <m@marceli.to>
2022-07-18 09:11:01 +0200

    fix links

--- resources/views/web/partials/menu.blade.php
+++ resources/views/web/partials/menu.blade.php
@@ -41,10 +41,10 @@
           </li>
         @endguest
         <li class="sm:hide">
-          <a href="">{{ __('Warenkorb') }}</a>
+          <a href="" title="{{ __('Warenkorb') }}">{{ __('Warenkorb') }}</a>
         </li>
         <li class="language-switcher sm:hide">
-          <a href="">
+          <a href="{{ app()->getLocale() == 'de' ? '/en' : '/de' }}">
             @if (app()->getLocale() == 'de')
               {{ __('English') }}
             @else
@@ -53,29 +53,29 @@
           </a>
         </li>
         <li class="language-switcher xs:hide">
-          <a href="">DE</a>
+          <a href="/de" title="{{ __('Deutsch') }}">DE</a>
           <span>&nbsp;/&nbsp;</span>
-          <a href="">EN</a>
+          <a href="/en" title="{{ __('English') }}">EN</a>
         </li>
       </ul>
     </div>
   </div>
   <footer class="site-menu__footer">
     <div>
-      <a href="mailto:kontakt@viak.ch" class="icon-mail" target="_blank">
+      <a href="mailto:kontakt@viak.ch" class="icon-mail" target="_blank" title="{{ __('Kontakt') }}">
         @include('web.partials.icons.mail')
       </a>
-      <a href="https://instagram.com" class="icon-instagram" target="_blank">
+      <a href="https://instagram.com" class="icon-instagram" target="_blank" title="{{ __('Viak auf Instagram') }}">
         @include('web.partials.icons.instagram')
       </a>
     </div>
     <div>
-      <a href="javascript:;" title="Menü" class="icon-menu icon-menu__cross js-menu-btn-hide">
+      <a href="javascript:;" title="Menü" class="icon-menu icon-menu__cross js-menu-btn-hide" title="{{ __('Menü anzeigen') }}">
         @include('web.partials.icons.cross', ['size' => 'large'])
       </a>
     </div>
   </footer>
 </nav>
-<a href="javascript:;" title="Menü" class="icon-menu icon-menu__burger js-menu-btn-show">
+<a href="javascript:;" title="Menü" class="icon-menu icon-menu__burger js-menu-btn-show" title="{{ __('Menu verbergen') }}">
   @include('web.partials.icons.burger')
 </a>
