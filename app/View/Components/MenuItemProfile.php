3bdad0bb07d2ce486025f03f39a94622388d5072
Marcel Stadelmann <m@marceli.to>
2022-07-19 23:32:26 +0200

    staging build

--- app/View/Components/MenuItemProfile.php
+++ app/View/Components/MenuItemProfile.php
@@ -1,17 +1,61 @@
 <?php
 namespace App\View\Components;
 use Illuminate\View\Component;
+use App\Models\Role;
 
 class MenuItemProfile extends Component
 {
+
+  /**
+   * Route per users role
+   * 
+   * @param String $route
+   */
+
+  public $route;
 
   /**
    * Create a new component instance.
    *
    * @return void
    */
-  public function __construct()
+  public function __construct($route = NULL)
   {
+    if (auth()->user())
+    {
+      if (auth()->user()->hasMultipleRoles())
+      {
+        if (session('user-selected-role'))
+        {
+          switch(session('user-selected-role')->key)
+          {
+            case 'student':
+              $route = 'page.student.profile';
+            break;
+            case 'expert':
+              $route = 'page.student.profile';
+            break;
+            case 'student':
+              $route = 'page.student.profile';
+            break;
+          }
+        }
+      }
+      else if (auth()->user()->hasRole(Role::find(Role::STUDENT)))
+      {
+        $route = 'page.student.profile';
+      }
+      else if (auth()->user()->hasRole(Role::find(Role::EXPERT)))
+      {
+        $route = 'page.student.profile';
+      }
+      else if (auth()->user()->hasRole(Role::find(Role::ADMIN)))
+      {
+        $route = 'page.student.profile';
+      }
+    }
+
+    $this->route = $route;
   }
 
   /**
--- resources/views/web/components/menu/profile.blade.php
+++ resources/views/web/components/menu/profile.blade.php
@@ -1,42 +1,18 @@
+<li>
 @auth
-  @if (auth()->user()->hasMultipleRoles())
-    @if (session('user-selected-role') && session('user-selected-role')->key == 'student')
-      <li>
-        <a href="{{ route('page.student.profile') }}" title="{{ __('Profil bearbeiten') }}">
-          <span class="sm:hide">{{ __('Profil') }}</span>
-          <span class="xs:hide icon-profile">
-            @include('web.partials.icons.profile')
-          </span>
-        </a>
-      </li>
-    @endif
-  @elseif (auth()->user()->hasRole(\App\Models\Role::find(\App\Models\Role::STUDENT)))
-    <li>
-      <a href="{{ route('page.student.profile') }}" title="{{ __('Profil bearbeiten') }}">
-        <span class="sm:hide">{{ __('Profil') }}</span>
-        <span class="xs:hide icon-profile">
-          @include('web.partials.icons.profile')
-        </span>
-      </a>
-    </li>
-  @else
-    <li>
-      <a href="" title="{{ __('Profil') }}">
-        <span class="sm:hide">{{ __('Profil') }}</span>
-        <span class="xs:hide icon-profile">
-          @include('web.partials.icons.profile')
-        </span>
-      </a>
-    </li>
-  @endif
+  <a href="{{ $route ? route($route) : '' }}" title="{{ __('Profil') }}">
+    <span class="sm:hide">{{ __('Profil') }}</span>
+    <span class="xs:hide icon-profile">
+      @include('web.partials.icons.profile')
+    </span>
+  </a>
 @endauth
 @guest
-  <li>
-    <a href="{{ route('login') }}" title="{{ __('Profil') }}">
-      <span class="sm:hide">{{ __('Profil') }}</span>
-      <span class="xs:hide icon-profile">
-        @include('web.partials.icons.profile')
-      </span>
-    </a>
-  </li>
+  <a href="{{ route('login') }}" title="{{ __('Profil') }}">
+    <span class="sm:hide">{{ __('Profil') }}</span>
+    <span class="xs:hide icon-profile">
+      @include('web.partials.icons.profile')
+    </span>
+  </a>
 @endguest
+</li>
