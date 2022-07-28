ff813e9d000ed521857dbc4572ab00a08d18734e
Marcel Stadelmann <m@marceli.to>
2022-07-28 08:47:26 +0200

    staging build

--- resources/views/web/components/menu/profile.blade.php
+++ resources/views/web/components/menu/profile.blade.php
@@ -3,7 +3,7 @@
   <a href="{{ $route ? route($route) : '' }}" title="{{ __('Profil') }}">
     <span class="sm:hide">{{ __('Profil') }}</span>
     <span class="xs:hide icon-profile">
-      @include('web.partials.icons.profile')
+      @include('web.partials.icons.profile', ['class' => 'is-active'])
     </span>
   </a>
 @endauth
