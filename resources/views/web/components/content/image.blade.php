1ee1777d0055eece5d0ea37f94cb7c11c90babd4
Marcel Stadelmann <m@marceli.to>
2022-07-29 14:07:52 +0200

    bf

--- resources/views/web/components/content/image.blade.php
+++ resources/views/web/components/content/image.blade.php
@@ -3,6 +3,7 @@
     @foreach($maxSizes as $minWidth => $maxSize)
       @if ($minWidth > 0)
         <source media="(min-width: {{ $minWidth }}px)" data-srcset="/img/cache/{{ $image->name }}/{{ $maxSize}}/{{ $image->coords }}/{{ $ratio }}">
+      @endif
     @endforeach
     <img 
     data-src="/img/cache/{{ $image->name }}/700/{{ $image->coords }}/{{ $ratio }}" 
