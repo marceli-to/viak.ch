2473277746e79f5756ac9b787803f5164884173c
Marcel Stadelmann <m@marceli.to>
2022-07-29 14:07:18 +0200

    bf

--- resources/views/web/components/content/image.blade.php
+++ resources/views/web/components/content/image.blade.php
@@ -3,16 +3,14 @@
     @foreach($maxSizes as $minWidth => $maxSize)
       @if ($minWidth > 0)
         <source media="(min-width: {{ $minWidth }}px)" data-srcset="/img/cache/{{ $image->name }}/{{ $maxSize}}/{{ $image->coords }}/{{ $ratio }}">
-      @else
-        <img 
-          data-src="/img/cache/{{ $image->name }}/{{ $maxSize }}/{{ $image->coords }}/{{ $ratio }}" 
-          src="/media/viak-placeholder-teaser.png"
-          width="{{ $width }}" 
-          height="{{ $height }}"
-          title="{{ $image->title }}"
-          class="is-responsive lazy"
-          alt="{{ $caption ?? '' }}">
-      @endif
     @endforeach
+    <img 
+    data-src="/img/cache/{{ $image->name }}/700/{{ $image->coords }}/{{ $ratio }}" 
+    src="/media/viak-placeholder-teaser.png"
+    width="{{ $width }}" 
+    height="{{ $height }}"
+    title="{{ $image->title }}"
+    class="is-responsive lazy"
+    alt="{{ $caption ?? '' }}">
   </picture>
 @endif
