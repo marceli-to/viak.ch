8d0e73e4b4288b3fc15a14ba391a6ac596b112c9
Marcel Stadelmann <m@marceli.to>
2022-07-30 17:42:23 +0200

    staging build

--- resources/js/vue/modules/dashboard/views/student/Form.vue
+++ resources/js/vue/modules/dashboard/views/student/Form.vue
@@ -11,67 +11,72 @@
       </div>
     </template>
     <template #content>
-        <form-group 
-          :label="'Geschlecht'" 
-          :required="true" 
-          :error="errors.gender_id"
-          v-if="isFetchedSettings">
-          <div class="select-wrapper">
-            <select v-model="data.gender_id" @change="removeError('gender_id')">
-              <option 
-                v-for="(option) in settings.genders" 
-                :key="option.id" 
-                :value="option.id">
-                {{option.description.de}}
-              </option>
-            </select>
-          </div>
+      <form-group 
+        :label="'Geschlecht'" 
+        :required="true" 
+        :error="errors.gender_id"
+        v-if="isFetchedSettings">
+        <div class="select-wrapper">
+          <select v-model="data.gender_id" @change="removeError('gender_id')">
+            <option 
+              v-for="(option) in settings.genders" 
+              :key="option.id" 
+              :value="option.id">
+              {{option.description.de}}
+            </option>
+          </select>
+        </div>
+      </form-group>
+      <form-group :label="'Vorname'" :required="true" :error="errors.firstname">
+        <input type="text" v-model="data.firstname" required @focus="removeError('firstname')" />
+      </form-group>
+      <form-group :label="'Name'" :required="true" :error="errors.name">
+        <input type="text" v-model="data.name" required @focus="removeError('name')" />
+      </form-group>
+      <form-group :label="'Telefon'">
+        <input type="text" v-model="data.phone" maxlength="30" />
+      </form-group>
+      <grid class="sm:grid-cols-12">
+        <form-group :label="'Strasse'" :required="true" class="span-6" :error="errors.street">
+          <input type="text" v-model="data.street" required @focus="removeError('street')" />
         </form-group>
-        <form-group :label="'Vorname'" :required="true" :error="errors.firstname">
-          <input type="text" v-model="data.firstname" required @focus="removeError('firstname')" />
+        <form-group :label="'Nr.'" class="span-6">
+          <input type="text" v-model="data.street_no" maxlength="5" />
         </form-group>
-        <form-group :label="'Name'" :required="true" :error="errors.name">
-          <input type="text" v-model="data.name" required @focus="removeError('name')" />
+      </grid>
+      <grid class="sm:grid-cols-12">
+        <form-group :label="'PLZ'" :required="true" class="span-6" :error="errors.zip">
+          <input type="text" v-model="data.zip" required maxlength="10" @focus="removeError('zip')" />
         </form-group>
-        <form-group :label="'Telefon'">
-          <input type="text" v-model="data.phone" maxlength="30" />
+        <form-group :label="'Ort'" :required="true" class="span-6" :error="errors.city">
+          <input type="text" v-model="data.city" required @focus="removeError('city')" />
         </form-group>
-        <grid class="sm:grid-cols-12">
-          <form-group :label="'Strasse'" :required="true" class="span-6" :error="errors.street">
-            <input type="text" v-model="data.street" required @focus="removeError('street')" />
-          </form-group>
-          <form-group :label="'Nr.'" class="span-6">
-            <input type="text" v-model="data.street_no" maxlength="5" />
-          </form-group>
-        </grid>
-        <grid class="sm:grid-cols-12">
-          <form-group :label="'PLZ'" :required="true" class="span-6" :error="errors.zip">
-            <input type="text" v-model="data.zip" required maxlength="10" @focus="removeError('zip')" />
-          </form-group>
-          <form-group :label="'Ort'" :required="true" class="span-6" :error="errors.city">
-            <input type="text" v-model="data.city" required @focus="removeError('city')" />
-          </form-group>
-        </grid>
-        <form-group class="has-underline" :error="errors.invoice_address">
-          <div class="flex items-center">
-            <input type="checkbox" id="has_invoice_address" name="has_invoice_address" required value="1" v-model="data.has_invoice_address">
-            <label for="has_invoice_address">
-              <em v-if="errors.invoice_address">Rechnungsadresse (abweichend) wird benötigt</em>
-              <em v-else>Rechnungsadresse (abweichend)</em>
-            </label>
-          </div>
-          <textarea 
-            v-model="data.invoice_address" 
-            :placeholder="'Rechnungsadresse'" 
-            class="is-plain mt-2x sm:mt-4x autosize" 
-            v-if="data.has_invoice_address">
-          </textarea>
-        </form-group>
-        <form-group>
-          <a href="" @click.prevent="submit()" :class="[isLoading ? 'is-disabled' : '', 'btn-primary']">
-            Speichern
-          </a>
-        </form-group>
+      </grid>
+      <form-group class="has-underline" :error="errors.invoice_address">
+        <div class="flex items-center">
+          <input type="checkbox" id="has_invoice_address" name="has_invoice_address" required value="1" v-model="data.has_invoice_address">
+          <label for="has_invoice_address">
+            <em v-if="errors.invoice_address">Rechnungsadresse (abweichend) wird benötigt</em>
+            <em v-else>Rechnungsadresse (abweichend)</em>
+          </label>
+        </div>
+        <textarea 
+          v-model="data.invoice_address" 
+          :placeholder="'Rechnungsadresse'" 
+          class="is-plain mt-2x sm:mt-4x autosize" 
+          v-if="data.has_invoice_address">
+        </textarea>
+      </form-group>
+      <form-group>
+        <a href="" @click.prevent="submit()" :class="[isLoading ? 'is-disabled' : '', 'btn-primary']">
+          Speichern
+        </a>
+      </form-group>
+      <div class="form-danger-zone" v-if="$props.type == 'edit'">
+        <h2>Student löschen</h2>
+        <p>Mit dieser Aktion wird der Student gelöscht.</p>
+        <a href="" class="btn-danger" @click.prevent="destroy()">Löschen</a>
+      </div>
     </template>
   </article-text>
 </form>
@@ -143,6 +148,7 @@ export default {
         find: '/api/dashboard/student',
         store: '/api/dashboard/student',
         update: '/api/dashboard/student',
+        delete: '/api/dashboard/student',
         settings: '/api/genders',
       },
 
@@ -206,7 +212,7 @@ export default {
       NProgress.start();
       this.isLoading = true;
       this.axios.post(this.routes.store, this.data).then(response => {
-        this.$router.push({ name: "students"});
+        this.$router.push({ name: 'students'});
         NProgress.done();
         this.isLoading = true;
       });
@@ -214,8 +220,20 @@ export default {
 
     update() {
       this.axios.put(`${this.routes.update}/${this.$route.params.id}`, this.data).then(response => {
-        this.$router.push({ name: "students"});
+        this.$router.push({ name: 'students'});
       });
+    },
+
+    destroy() {
+      if (confirm('Sicher?')) {
+        this.isLoading = true;
+        NProgress.start();
+        this.axios.delete(`${this.routes.delete}/${this.data.id}`).then(response => {
+          this.$router.push({ name: 'students'});
+          this.isLoading = false;
+          NProgress.done();
+        });
+      }
     },
 
     sorted(data, by, dir){
