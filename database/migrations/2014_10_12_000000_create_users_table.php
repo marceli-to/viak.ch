f7920fb04848db560e58ea0e30c8b7286f5edbee
Marcel Stadelmann <m@marceli.to>
2022-10-11 17:00:32 +0200

    add migration for country id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,6 @@ class CreateUsersTable extends Migration
       $table->string('password');
       $table->string('uuid', 36);
       $table->foreignId('gender_id')->constrained();
-      $table->foreignId('country_id')->constrained();
       $table->rememberToken();
       $table->timestamps();
       $table->softDeletes();
