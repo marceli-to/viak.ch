f0bee3ab560c7915c25c9f589435b95fc284d9ee
Marcel Stadelmann <m@marceli.to>
2022-10-13 16:40:43 +0200

    yet another update

--- resources/views/mail/user/expert/create.blade.php
+++ resources/views/mail/user/expert/create.blade.php
@@ -2,7 +2,7 @@
 <h1>{{ __('Zugangsdaten') }}</h1>
 <p>{{ __('Hallo') }} {{ $data->firstname }} {{ $data->name }}</p>
 <p>{{ __('Es wurde ein Konto für Dich eingerichtet. Um die Erstellung Deines Kontos abzuschliessen, müssen wir Deine E-Mail-Adresse verifizieren. Bitte klick auf folgenden Button:') }}</p>
-<p><a href="{{ $confirmUrl }}" class="button button-primary">{{ __('E-Mail Bestätigen') }}</a></p>
+<p class="py-2x"><a href="{{ $confirmUrl }}" class="button button-primary">{{ __('E-Mail Bestätigen') }}</a></p>
 <p><small>{{ __('Sollte der Button "E-Mail Bestätigen" nicht funktionieren, kopier bitte die untenstehende URL und fügen sie in die Adresszeile Deines Browses ein:') }}<br><span class="break-all">{{ $confirmUrl }}</span></small></p>
 @include('mail.partials.signature')
 @endcomponent
