c8924d9c268722c7d6616fb43c9737772adae0fe
Marcel Stadelmann <m@marceli.to>
2022-10-13 16:56:38 +0200

    yet another update

--- resources/views/mail/user/student/register.blade.php
+++ resources/views/mail/user/student/register.blade.php
@@ -2,7 +2,7 @@
 <h1>{{ __('Bestätigung Anmeldung') }}</h1>
 <p>{{ __('Hallo') }} {{ $data->firstname }} {{ $data->name }}</p>
 <p>{{ __('Vielen Dank für Deine Anmeldung bei der Visualisierungs-Akademie Schweiz GmbH.') }}</p>
-<p>{{ __('Um Deine Registration abzuschliessen, musst Du noch Deine E-Mail-Adresse bestätigen. Klicke dafür bitte auf folgenden Button::') }}</p>
+<p>{{ __('Um Deine Registration abzuschliessen, musst Du noch Deine E-Mail-Adresse bestätigen. Klicke dafür bitte auf folgenden Button:') }}</p>
 <p class="py-2x"><a href="{{ $verifyUrl }}" class="button button-primary">{{ __('E-Mail Bestätigen') }}</a></p>
 @include('mail.partials.signature')
 <p><small>{{ __('Sollte der Button "E-Mail Bestätigen" nicht funktionieren, kopier bitte die untenstehende URL und fügen sie in die Adresszeile Deines Browses ein:') }}<br><span class="break-all">{{ $verifyUrl }}</span></small></p>
