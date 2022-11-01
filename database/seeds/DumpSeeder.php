a5c41415fc751a4d189ee662edd3da8ae49c6a57
Marcel Stadelmann <m@marceli.to>
2022-11-01 10:29:32 +0100

    update dump seeder

--- database/seeds/DumpSeeder.php
+++ database/seeds/DumpSeeder.php
@@ -18,9 +18,19 @@ class DumpSeeder extends Seeder
         'username' => env('DB_USERNAME'),
         'password' => env('DB_PASSWORD'),
         'host' => env('DB_HOST'),
-        'database' => env('DB_DATABASE')
+        'database' => env('DB_DATABASE'),
+        'socket' => env('DB_SOCKET')
     ];
-    exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
+
+    if (app()->environment() == 'local')
+    {
+      exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} --socket {$db['socket']} < $sql");
+    }
+    else
+    {
+      exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
+    }
+
     \Log::info('SQL Import Done');
   }
 }
