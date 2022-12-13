27074e6ef143ac73bc01118af7f2a178a8500213
Marcel Stadelmann <m@marceli.to>
2022-12-13 15:52:35 +0100

    fixes bug in mail

--- app/Mail/BookingCancelledWithPenalty.php
+++ app/Mail/BookingCancelledWithPenalty.php
@@ -53,7 +53,7 @@ class BookingCancelledWithPenalty extends Mailable
     if ($invoice)
     {
       $mail->attach(
-        public_path() . '/storage/files/' . $invoice->filename,
+        public_path() . "/storage/files/{$booking->user->uuid}/{$invoice->filename}",
         ['mime' => 'application/pdf']
       );
     }
