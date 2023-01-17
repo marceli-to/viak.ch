e023b3cbee916c3f16bc7587ec39e78926161fd7
Marcel Stadelmann <m@marceli.to>
2023-01-17 16:10:17 +0100

    wip

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -1,4 +1,5 @@
 <script src="{{ mix('assets/js/app.js') }}" type="text/javascript"></script>
+<script src="https://apps.elfsight.com/p/platform.js"></script>
 </body>
 <!-- made with ❤ by wbg.ch & marceli.to -->
 </html>
