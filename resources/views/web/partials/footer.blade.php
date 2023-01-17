b18b0e275738457b9248b84780cda00edb80387b
Marcel Stadelmann <m@marceli.to>
2023-01-17 16:15:29 +0100

    staging build

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -1,5 +1,4 @@
 <script src="{{ mix('assets/js/app.js') }}" type="text/javascript"></script>
-<script src="https://apps.elfsight.com/p/platform.js"></script>
 </body>
 <!-- made with ❤ by wbg.ch & marceli.to -->
 </html>
