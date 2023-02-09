81eb7ff7ecbf9a827f6b6af6dee07bbaba9e92d2
Marcel Stadelmann <m@marceli.to>
2023-02-09 17:14:29 +0100

    wip

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -55,7 +55,7 @@ class ImageController extends ImageCacheController
 
     // filter template found
     case class_exists($template):
-      return new $template($this->maxSize, $this->coords);
+      return new $template($this->maxSize, $this->coords, $this->ratio);
 
     default:
       // template not found
