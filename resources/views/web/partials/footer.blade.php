c20766bc1fc38b10b540ff0def5c3bdbc3fc7c9b
Marcel Stadelmann <m@marceli.to>
2023-02-09 16:07:15 +0100

    wip

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -21,9 +21,9 @@
     </div>
   </div>
 </footer>
-<script src="{{ mix('assets/js/global/newsletter.js') }}" type="text/javascript"></script>
+<script src="{{ mix('assets/js/global/newsletter.js') }}" defer></script>
 @endif
-<script src="{{ mix('assets/js/app.js') }}" type="text/javascript"></script>
+<script src="{{ mix('assets/js/app.js') }}"></script>
 @production
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M3L7WVP');</script>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3L7WVP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
