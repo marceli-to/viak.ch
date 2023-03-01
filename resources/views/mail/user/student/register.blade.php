a25a6f7c8ad282b47d403a22427e31b9021e86f9
Marcel Stadelmann <m@marceli.to>
2023-03-01 12:25:48 +0100

    update text in registration mail

--- resources/views/mail/user/student/register.blade.php
+++ resources/views/mail/user/student/register.blade.php
@@ -1,9 +1,10 @@
 @component('mail::message')
-<h1>{{ __('Bestätigung Anmeldung') }}</h1>
+<h1>{{ __('Bestätigung Account') }}</h1>
 <p>{{ __('Guten Tag') }} {{ $data->firstname }} {{ $data->name }}</p>
-<p>{{ __('Vielen Dank für Deine Anmeldung bei der Visualisierungs-Akademie Schweiz GmbH.') }}</p>
-<p>{{ __('Um Deine Registration abzuschliessen, musst Du noch Deine E-Mail-Adresse bestätigen. Klicke dafür bitte auf folgenden Button:') }}</p>
+<p>{{ __('Vielen Dank für Dein Interesse an der der Visualisierungs-Akademie Schweiz GmbH.') }}</p>
+<p>{{ __('Um die Registration Deines Accounts abzuschliessen, musst Du noch Deine E-Mail-Adresse bestätigen. Klicke dafür bitte auf folgenden Button:') }}</p>
 <p class="py-2x"><a href="{{ $verifyUrl }}" class="button button-primary">{{ __('E-Mail Bestätigen') }}</a></p>
+<p>{{ __('Sobald Du das erledigt hast, kannst Du bei uns Deinen ersten Kurs buchen.') }}
 @include('mail.partials.signature')
 <p><small>{{ __('Sollte der Button "E-Mail Bestätigen" nicht funktionieren, kopier bitte die untenstehende URL und fügen sie in die Adresszeile Deines Browses ein:') }}<br><span class="break-all">{{ $verifyUrl }}</span></small></p>
 @endcomponent
