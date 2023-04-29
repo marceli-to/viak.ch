32317c30e784f1c24a1fb20a19d70a4aebfa3303
Marcel Stadelmann <m@marceli.to>
2023-04-29 11:06:55 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,10 @@ class AppServiceProvider extends ServiceProvider
    */
   public function register()
   {
-    if ($this->app->isLocal())
-    {
-      $this->app->register(\Amirami\Localizator\ServiceProvider::class);
-    }
+    // if ($this->app->isLocal())
+    // {
+    //   $this->app->register(\Amirami\Localizator\ServiceProvider::class);
+    // }
   }
 
   /**
