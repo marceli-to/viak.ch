81593ba1112ec7ec7af6e1405f6d00f71ef099c1
Marcel Stadelmann <m@marceli.to>
2023-06-29 16:57:02 +0200

    wip

--- app/Exports/Sheets/CourseExportSheet.php
+++ app/Exports/Sheets/CourseExportSheet.php
@@ -28,32 +28,6 @@ class CourseExportSheet implements FromCollection, WithTitle, WithHeadings, Shou
 
     $data = [];
 
-    if ($course->pastEvents->count() > 2) {
-      $data2 = [];
-      foreach ($course->pastEvents as $event) {
-        echo $event->date;
-        echo '<br>';
-        echo 'bookings count: ' . $event->bookings->count();
-        echo '<br>';
-        foreach ($event->bookings as $booking) {
-          $data2[] = [
-            'firstname' => $booking->user->firstname,
-            'name' => $booking->user->name,
-            'company' => $booking->user->company,
-            'street' => $booking->user->street,
-            'street_no' => $booking->user->street_no,
-            'zip' => $booking->user->zip,
-            'city' => $booking->user->city,
-            'phone' => $booking->user->phone,
-            'email' => $booking->user->email,
-            'date' => $booking->event->date
-          ];
-        }
-      }
-    
-      dd($data2);
-    }
-
     foreach($course->pastEvents as $event)
     {
       foreach($event->bookings as $booking)
