8f86babb84b27936d2a7f767524c00fc8ec8976e
Marcel Stadelmann <m@marceli.to>
2023-06-29 16:50:55 +0200

    wip

--- app/Exports/Sheets/CourseExportSheet.php
+++ app/Exports/Sheets/CourseExportSheet.php
@@ -28,10 +28,30 @@ class CourseExportSheet implements FromCollection, WithTitle, WithHeadings, Shou
 
     $data = [];
 
-    // if ($course->pastEvents->count() > 2)
-    // {
-    //   dd($course->pastEvents);
-    // }
+    if ($course->pastEvents->count() > 2)
+    {
+      $data2 = [];
+      foreach($course->pastEvents as $event)
+      {
+        foreach($event->bookings as $booking)
+        {
+          $data[] = [
+            'firstname' => $booking->user->firstname,
+            'name' => $booking->user->name,
+            'company' => $booking->user->company,
+            'street' => $booking->user->street,
+            'street_no' => $booking->user->street_no,
+            'zip' => $booking->user->zip,
+            'city' => $booking->user->city,
+            'phone' => $booking->user->phone,
+            'email' => $booking->user->email,
+            'date' => $booking->event->date
+          ];
+        }
+      }
+
+      dd($data2);
+    }
 
     foreach($course->pastEvents as $event)
     {
