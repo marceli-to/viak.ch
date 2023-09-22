fb443c0efffc687f3e9a4fa912f52676c02bc22d
Marcel Stadelmann <m@marceli.to>
2023-09-22 10:14:54 +0200

    wip

--- resources/views/web/pages/events/components/card.blade.php
+++ resources/views/web/pages/events/components/card.blade.php
@@ -45,7 +45,7 @@
         {{ __('Onlinekurs') }}
       @else
         @if ($event->location->map)
-          Kursort: 
+          Kursort:<br>
           <a href="{{ $event->location->map }}" target="_blank" title="{{ __('Karte anzeigen') }}">
             {{ $event->location->description }}
           </a>
